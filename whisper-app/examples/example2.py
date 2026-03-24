8aa2d3067a90d2cdf8088ea367870a1bc77ce36c
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-24 19:46:31 -0400

    Simple Transcriber Scripts
    -Example Transcriber Python scripts using the
    OpenAI Whisper API.

---
+++ whisper-app/examples/example2.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(api_key = os.getenv('API_KEY'))
+
+file_path = "../audio/rus_sample.wav"
+
+audio_file = open(file_path, "rb")
+
+transcription = client.audio.transcriptions.create(model = "whisper-1", file = audio_file)
+
+print(transcription.text)
