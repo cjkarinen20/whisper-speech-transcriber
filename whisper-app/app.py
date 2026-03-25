fc33cc8052a9b0c3520331831512669548a93df9
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-24 20:05:50 -0400

    Initialized Flask app
    -Installed Flask into the venv.
    -Created 'app.py'.
    -Created the html template for the main page.

---
+++ whisper-app/app.py
@@ -0,0 +1,13 @@
+import os 
+from flask import Flask, request, jsonify, render_template
+from openai import OpenAI
+
+app = Flask(__name__)
+client = OpenAI(api_key = os.getenv('API_KEY'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5123, debug = True)
